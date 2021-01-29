3267dd88732e172ebd50b8615640a230cff3482c
agBAZE <drakitoo94@gmail.com>
2021-01-29 13:07:38 -0400

    Se agrega información al archivo "index.php"

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Store</title>
+</head>
+<body>
+    <?php
+    echo "<h1>¡Bienvenido!</h1>";
+    ?>
+</body>
+</html>
